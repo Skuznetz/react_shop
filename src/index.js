97f81f62fb5cbafe87d54e53f53a0258e077002a
Skuznetz <s.kuznetzov@mail.ru>
2018-09-13 21:13:37 +0400

    импортируем reducers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import App from './App';
+import reducers from 'reducers'
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
