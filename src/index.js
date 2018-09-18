6d669017f5deb4aba56a4d931dd1d0a064f1a460
Skuznetz <s.kuznetzov@mail.ru>
2018-09-18 22:47:33 +0400

    уберём лишнее

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import reducers from 'reducers'
-import registerServiceWorker from './registerServiceWorker';
+
 
 const store = createStore(reducers)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
