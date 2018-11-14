ebc5a133a59ff5e0f2ac0dc5646ac500bb49140c
Skuznetz <s.kuznetzov@mail.ru>
2018-11-14 18:37:11 +0300

    импортируем compose

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import reducers from 'reducers'
 import thunk from 'redux-thunk'
-
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 
 const store = createStore(reducers)
