508a1f20cd75cfd46ecd874a6011a3316a3715fe
Skuznetz <s.kuznetzov@mail.ru>
2018-11-14 18:35:10 +0300

    импортируем thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import reducers from 'reducers'
+import thunk from 'redux-thunk'
+
 
 
 const store = createStore(reducers)
