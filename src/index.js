3bdd9336c1565ca03e1cd91a0c178c3e98aa4e0a
Skuznetz <s.kuznetzov@mail.ru>
2018-11-15 18:59:28 +0300

    browserHistory

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import {createStore,applyMiddleware} from 'redux'
 import reducers from 'reducers'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {browserHistory} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
 
 const store = createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
+const history = syncHistoryWithStore(browserHistory,store)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
