aac7142d9b2692acdddee273dfae0e6e915015d7
Skuznetz <s.kuznetzov@mail.ru>
2018-11-15 19:55:00 +0300

    изменим react-router

--- src/index.js
+++ src/index.js
@@ -6,22 +6,22 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
 
-import {browserHistory} from 'react-router'
+import {BrowserRouter} from 'react-router-dom'
 import {syncHistoryWithStore} from 'react-router-redux'
 import {Router,Route} from 'react-router'
 import Layout from 'containers/layout'
 import Books from 'containers/books'
-
+import createBrowserHistory from "history/createBrowserHistory"
 const store = createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
-const history = syncHistoryWithStore(browserHistory,store)
+const history = createBrowserHistory()
 
 ReactDOM.render(
     <Provider store = {store}>
-        <Router history = {history}>
+        <BrowserRouter history = {history}>
             <Route component = {Layout}>
                 <Route path = '/' component = {Books} />
             </Route>
-        </Router>
+        </BrowserRouter>
     </Provider>,
 document.getElementById('root')    
 )
