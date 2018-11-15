f890c5b910edc432268a3e514ff30ea9a36c9d6b
Skuznetz <s.kuznetzov@mail.ru>
2018-11-15 19:10:34 +0300

    импортируем Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {createStore,applyMiddleware} from 'redux'
 import reducers from 'reducers'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
+
 import {browserHistory} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
 
